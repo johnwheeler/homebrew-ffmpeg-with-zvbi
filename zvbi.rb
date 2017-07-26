57b02dab1932d8e46ea9dfdb9a9ed36b80d73577
johnwheeler <john@johnwheeler.org>
2017-07-26 12:49:21 -0700

    .

--- zvbi.rb
+++ zvbi.rb
@@ -3,7 +3,7 @@ require "formula"
 class Zvbi < Formula
   homepage 'http://zapping.sourceforge.net/'
   url "http://downloads.sourceforge.net/project/zapping/zvbi/0.2.35/zvbi-0.2.35.tar.bz2"
-  sha1 "b0fc8d596c90d603e883e6b195318c6b276a3eb4"
+  sha256 "fc883c34111a487c4a783f91b1b2bb5610d8d8e58dcba80c7ab31e67e4765318"
 
   # two of the unit tests fail on Mac OS - see
   # http://sourceforge.net/p/zapping/bugs/197/ - so remove them until
